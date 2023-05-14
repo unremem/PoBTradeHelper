7207041aa08696e92868a0cca5709bbb804f2481
unremem <unremem@gmail.com>
2023-05-14 15:14:37 -0700

    Attach embed version of python to make the host script portable
    Also restructure the repo to make things a bit easier to organize

---
+++ client/pob-trade-helper-native-host.bat
@@ -0,0 +1,5 @@
+@echo off
+:: Copyright (c) 2013 The Chromium Authors. All rights reserved.
+:: Use of this source code is governed by a BSD-style license that can be
+:: found in the LICENSE file.
+"%~dp0/python-3.10.11-embed-amd64/python.exe" "%~dp0/pob-trade-helper-native-host" %*
